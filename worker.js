8866aa7a4fb8e1ca8ada8f599fb21639280e01f3
Robin.Wang <3Jg$94sbQ@q1>
2024-04-09 09:36:45 +0800

    change video permission call; add prettier

--- worker.js
+++ worker.js
@@ -1,18 +1,23 @@
-importScripts("https://cdn.jsdelivr.net/npm/onnxruntime-web@1.17.1/dist/ort.min.js");
+importScripts(
+  "https://cdn.jsdelivr.net/npm/onnxruntime-web@1.17.1/dist/ort.min.js",
+);
 
 let model = null;
 
 async function run_model(input) {
-    if (!model) {
-        model = await ort.InferenceSession.create("./rps_best.onnx",{ executionProviders: ['wasm'], graphOptimizationLevel: 'all' });
-    }
-    input = new ort.Tensor(Float32Array.from(input),[1, 3, 640, 640]);
-    const outputs = await model.run({images:input});
-    return outputs["output0"].data;
+  if (!model) {
+    model = await ort.InferenceSession.create("./rps_best.onnx", {
+      executionProviders: ["wasm"],
+      graphOptimizationLevel: "all",
+    });
+  }
+  input = new ort.Tensor(Float32Array.from(input), [1, 3, 640, 640]);
+  const outputs = await model.run({ images: input });
+  return outputs["output0"].data;
 }
 
-onmessage = async(event) => {
-    const input = event.data;
-    const output = await run_model(input);
-    postMessage(output);
-}
+onmessage = async (event) => {
+  const input = event.data;
+  const output = await run_model(input);
+  postMessage(output);
+};
